7eeed18681c00cb9b56cb6c1aab317d78e559d7c
TWKramer <tkramerwfs@gmail.com>
2023-01-17 03:33:27 -0600

    update main menu

--- index.js
+++ index.js
@@ -1,124 +1,136 @@
-let isMainMenuOpen = true;
-let isNamePageOpen = false;
-let isGameplayOpen = false;
-let isGameResultPageOpen = false;
-let stateOfPage = false
 
-// the next 4 if statements represent the 4 pages I have coded into the game.
-//main menu, enter your name, play the game, did you win or not... those
-//are the 4 UI's basically
-
-if (isMainMenuOpen = true){
-isNamePageOpen = false;
-isGameplayOpen = false;
-isGameResultPageOpen = false;
-  
-}
-
-//above is the state of the user in the main menu. Page One
-
-if (isNamePageOpen = true){
-isMainMenuOpen = false;
-isGameplayOpen = false;
-isGameResultPageOpen = false;
-  
-}
-
-//above is the state of the user in the page where you
-//create a name for your character. Page Two
-
-
-if (isGameplayOpen = true){
-isMainMenuOpen = false;
-isNamePageOpen = false;
-isGameResultPageOpen = false;
-  
-}
-
-//above is the state of the user acutally playing the game Page Three
-
-
-if (isGameResultPageOpen = true){
-isMainMenuOpen = false;
-isNamePageOpen = false;
-isGameplayOpen = false;
-
-}
-
-//above is the state of the user has finished the game and win loss result is being show Page Four
 
 function newGameFunction() {
-   isMainMenuOpen = false;
-   isNamePageOpen = true;
-    if (isMainMenuOpen === false){
-      var mainMenu = document.getElementById("mainMenuContainer")
-      mainMenu.style.display = "none";
-    if (isNamePageOpen === true){
-      var namePage = document.getElementById("nameContainer")
-      namePage.style.display = "block";
+   fromMenuToNamePage = true;
+    if (fromMenuToNamePage === true){
+    let nameContainer = document.getElementById("nameContainer")
+    nameContainer.style.display = "block";
+    let mainMenuContainer = document.getElementById("mainMenuContainer")
+    mainMenuContainer.style.display = "none";
+    let nameForm1 = document.getElementById("nameForm1")
+    nameForm1.style.display = "block";
+    let namePageButtons = document.getElementById("namePageButtons")
+    namePageButtons.style.display = "block";
+
     }
-}
 }
 //below is the function i've added which is called after you enter the name of your character
 //and you are ready to begin the game... this function will save input info, close the namePage, and begin the gameplay all in one click
 function createPlayerAndBeginFunction() {
-   isNamePageOpen = false;
+   inGamePage = false;
    isGameplayOpen = true;
+   isMainMenuOpen = false;
     if (isMainMenuOpen === false){
-      namepage = document.getElementById("nameContainer")
-      namePage.style.display = "none";
+      nameContainer = document.getElementById("nameContainer")
+      nameContainer.style.display = "none";
       if (isGameplayOpen === true){
-      var gameplayPage = document.getElementById("playingContainer")
-      gameplayPage.style.display = "block";
+      var inGamePage = document.getElementById("inGamePage")
+      inGamePage.style.display = "block";
+      let inGameButtons = document.getElementById("inGameButtons")
+      inGameButtons.style.display = "block";
     }
 }
 }
+//add a pause button somewhere
+
+
+
+function quitFunction1() {
+    quittingPrompted = true;
+  if (quittingPrompted === true){
+    let namePageButtons = document.getElementById("namePageButtons")
+    namePageButtons.style.display = "none";
+    let nameForm1 = document.getElementById("nameForm1")
+    nameForm1.style.display = "none";
+    let gamePausedAlert1 = document.getElementById("gamePausedAlert1")
+    gamePausedAlert1.style.display = "block";
+    let quitMessageBox1 = document.getElementById("quitMessageBox1")
+    quitMessageBox1.style.display = "block";
+    let quitMessage1 = document.getElementById("quitMessage1")
+    quitMessage1.style.display = "block";
+  }
+}
+
+function yesQuitFunction1() {
+  selectedyesQuitFunction1 = true
+if (selectedyesQuitFunction1 === true){
+  let gamePausedAlert1 = document.getElementById("gamePausedAlert1")
+  gamePausedAlert1.style.display = "none";
+  let quitMessageBox1 = document.getElementById("quitMessageBox1")
+  quitMessageBox1.style.display = "none";
+  let quitMessage1 = document.getElementById("quitMessage1")
+  quitMessage1.style.display = "none";
+  let mainMenuContainer = document.getElementById("mainMenuContainer")
+  mainMenuContainer.style.display = "block";
+
+
+}
+}
+
+function dontQuitFunction1() {
+  selectedDontQuitFunction = true;
+if (selectedDontQuitFunction === true){
+  let namePageButtons = document.getElementById("namePageButtons")
+  namePageButtons.style.display = "block";
+  let nameForm1 = document.getElementById("nameForm1")
+  nameForm1.style.display = "block";
+  let gamePausedAlert1 = document.getElementById("gamePausedAlert1")
+  gamePausedAlert1.style.display = "none";
+  let quitMessageBox1 = document.getElementById("quitMessageBox1")
+  quitMessageBox1.style.display = "none";
+  let quitMessage1 = document.getElementById("quitMessage1")
+  quitMessage1.style.display = "none";
+
+ }
+}
+function quitFunction2() {
+    quittingPrompted = true;
+  if (quittingPrompted === true){
+    let gamePausedAlert2 = document.getElementById("gamePausedAlert2")
+    gamePausedAlert2.style.display = "block";
+    let quitMessageBox2 = document.getElementById("quitMessageBox2")
+    quitMessageBox2.style.display = "block";
+    let quitMessage2 = document.getElementById("quitMessage2")
+    quitMessage2.style.display = "block";
+    let inGameButtons = document.getElementById("inGameButtons")
+    inGameButtons.style.display = "none";
   
 
-/*
-function newGameFunction() {
-  var newGame = document.getElementById("mainMenuContainer");
-    if (newGame.style.display === "none") {
-      newGame.style.display = "block";
-    } else {
-      newGame.style.display = "none";
-      nameContainer.style.display = "block";
-    }
-  }
-function nameMenuStartGameFunction() {
-  var startGame = document.getElementById("nameContainer");
-    if (startGame.style.display === "none") {
-      startGame.style.display = "block";
-    } else {
-      startGame.style.display = "none";
-      playingContainer.style.display = "block";
-    }
-  }
-  exitFunction
-  function exitFunction() {
-    var exitGame = document.getElementById("mainMenuContainer");
-      if (exitGame.style.display === "none") {
-        exitGame.style.display = "block";
-      } else {
-        exitGame.style.display = "none";
-        nameContainer.style.display = "none";
-      }
-    }
-
-*/
-/*
-function myFunction() {
-  document.getElementById("newGamebtn").onclick = function(){
-
-    alert("display of Main Menu is '" + mainMenuContainer.style.display + "'");
-    alert("display of Name Page is '" + nameContainer.style.display + "'");
-    alert("display of Game Page is '" + playingContainer.style.display + "'");
     
-    document.getElementById("mainMenuContainer").style.display="none";
-    document.getElementById("nameContainer").style.display="block";
-    document.getElementById("playingContainer").style.display="none";
+  }
+  }
 
 
-}
-}
-*/
+function dontQuitFunction2() {
+    selectedDontQuitFunction = true;
+  if (selectedDontQuitFunction === true){
+    let gamePausedAlert2 = document.getElementById("gamePausedAlert2")
+    gamePausedAlert2.style.display = "none";
+    let quitMessageBox2 = document.getElementById("quitMessageBox2")
+    quitMessageBox2.style.display = "none";
+    let quitMessage2 = document.getElementById("quitMessage2")
+    quitMessage2.style.display = "none";
+    let inGameButtons = document.getElementById("inGameButtons")
+    inGameButtons.style.display = "block";
+  
+
+   }
+  }
+  function yesQuitFunction2() {
+    selectedyesQuitFunction2 = true
+  if (selectedyesQuitFunction2 === true){
+    let gamePausedAlert2 = document.getElementById("gamePausedAlert2")
+    gamePausedAlert2.style.display = "none";
+    let quitMessageBox2 = document.getElementById("quitMessageBox2")
+    quitMessageBox2.style.display = "none";
+    let quitMessage2 = document.getElementById("quitMessage1")
+    quitMessage2.style.display = "none";
+    let mainMenuContainer = document.getElementById("mainMenuContainer")
+    mainMenuContainer.style.display = "block";
+    let inGameButtons = document.getElementById("inGameButtons")
+    inGameButtons.style.display = "none";
+  
+  
+  }
+  }
+
